48d99cfa2b61b9727d014936de58f3715cc09005
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2021-09-19 16:26:39 +0200

    create desc len plot and add show data file

--- data_preparation.py
+++ data_preparation.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import torch
 from transformers import pipeline
 
 # load data
@@ -13,11 +14,13 @@ print("Cleaning data...")
 # format time string to datetime obj and drop string from df
 data["date_and_time"] = pd.to_datetime(data["time_text"], format="%d.%m.%Y %H:%M Uhr")
 data.drop(columns=["time_text"], inplace=True)
+data.reset_index(inplace=True)
+data["sendung_id"] = data.index
 
 # format time strings to datetime
 data["date"] = pd.to_datetime(data["date"], format="%d/%m/%Y")
 
-# override date on etries where date and proofdate differ
+# override date on entries where date and proofdate differ
 data["proofdate"] = data["date_and_time"].dt.date
 data["date"] = data["proofdate"]
 data.drop(columns=["proofdate"], inplace=True)
@@ -27,9 +30,9 @@ print("Done")
 print("Adding Features...")
 data["year"] = data["date_and_time"].dt.year
 data["month"] = data["date_and_time"].dt.month
-data["quarter"] = data["date_and_time"].dt.quarter.astype(str) + "/" + data["year"].astype(str)
+data["quarter"] = data["year"].astype(str) + "/" + data["date_and_time"].dt.quarter.astype(str)
 data["day"] = data["date_and_time"].dt.day
-data["weekday"] = data["date_and_time"].dt.dayofweek
+data["weekday"] = data["date_and_time"].dt.day_name()
 data["timeslot"] = data["date_and_time"].dt.strftime("%H%M").astype(int) - data["date_and_time"].dt.strftime("%H%M").astype(int) % 20
 
 data.drop_duplicates(
@@ -49,6 +52,11 @@ data = data.merge(episodes_that_day, how="left", on="date")
 data["article"] = data["article"].str.split(",")
 data["num_topics"] = data["article"].str.len()
 
+# format title column so they are rightly classified
+data["title"] = data["title"].str.strip()
+
+data.to_excel("data/sendungen.xlsx", index=False)
+
 # unstack "article" column to every topic
 stacked_column = "article"
 other_columns = data.columns.difference([stacked_column])
@@ -60,9 +68,9 @@ unstacked_df = {
 unstacked_df["topic"] = all_topics
 data = pd.DataFrame(unstacked_df)
 
-# format title column so they are rightly classified
-data["title"] = data["title"].str.lower().str.replace(" ", "")
-# cast topic column to str
+# cast topic column to str and drop missing entries
+data.dropna(subset=["topic"], inplace=True)
+data = data[data["topic"] != ""]
 data["topic"] = data["topic"].astype("str")
 print("Done")
 
@@ -71,22 +79,31 @@ tqdm.pandas()
 
 # load model
 print("Loading Model...")
-classifier = pipeline("zero-shot-classification", model="Sahajtomar/German_Zeroshot", device=0)
+classifier = pipeline(
+    "zero-shot-classification", 
+    model="Sahajtomar/German_Zeroshot", 
+    device=torch.cuda.current_device()
+    )
+print("Done")
 
 categories = [
     "Politik",
     "Wirtschaft",
-    "Lottozahlen",
     "Sport",
     "Naturkatastrophe",
     "Kunst und Kultur",
-    "Terrorismus"
-    "Pandemie"
+    "Terrorismus",
+    "Lottozahlen"
 ]
 
 def categorise_topic(topic_str: str) -> str:
     """Categorizes Topic via NLP"""
-    model_output = classifier(topic_str, categories)
+    try:
+        model_output = classifier(topic_str, categories)
+    except ValueError:
+        print(topic_str)
+        print(topic_str)
+        print(type(topic_str))
     labels = model_output["labels"]
     output = model_output["scores"]
     category_index = output.index(max(output))
@@ -98,5 +115,5 @@ print("Done")
 
 # save to file
 print("Saving to file...")
-data.to_excel("data/final_format.xlsx", index=False)
+data.to_excel("data/topics.xlsx", index=False)
 print("Done")
